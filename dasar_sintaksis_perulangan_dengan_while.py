124429edf2a5d1d102a6dd80640f248cd9cd1459
Lenovo <ferdi1505@gmail.com>
2022-07-12 08:36:41 +0700

    basic syntact for and while

---
+++ dasar_sintaksis_perulangan_dengan_while.py
@@ -0,0 +1,12 @@
+# Dasar Sintaksis perulangan membaca buku
+
+jumlah_buku = 100
+print('Ibu berkata, "baca semua bukumu"')
+
+jumlah_buku_yang_sudah_dibaca = 0
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+
+for jumlah_buku_yang_sudah_dibaca in range(1,jumlah_buku+1) :
+    print(f"Buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+
+print(f"jumlah buku yang sudah di baca {jumlah_buku_yang_sudah_dibaca}")
